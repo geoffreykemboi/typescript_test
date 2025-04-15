9be4a6cc001354b855180aa3ebc5ed19163ec4fd
Geoffreykemboi <geoffreyk126@gmail.com>
2025-04-15 21:39:16 +0300

    typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function greetAnyone(name) {
+    return `hello ${name}`;
+}
+const newName = "Geoffrey";
+console.log(greetAnyone(newName));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greetAnyone(name:string) : string {
+         return `hello ${name}`;
+}
+const newName : string = "Geoffrey";
+console.log(greetAnyone(newName));
